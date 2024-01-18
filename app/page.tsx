33dc81158886e25deae9a508536386ac1256d16c
Hajin Lee <betterthanhajin@icloud.com>
2024-01-18 19:19:21 +0900

    fix: 잘못된 위치 바로 잡는중

--- app/page.tsx
+++ app/page.tsx
@@ -1,50 +1,92 @@
 "use client";
 import Image from "next/image";
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
 
 export default function Home() {
-  const rows = 30; // 행의 수
-  const cols = 30; // 열의 수
-
-  const [rotation, setRotation] = useState(
-    Array(rows).fill(Array(cols).fill(0))
+  const rowNumber = 30; // 행의 수
+  const colNumber = 30; // 열의 수
+  const [rotations, setRotations] = useState(
+    Array(rowNumber).fill(Array(colNumber).fill(0))
   );
+  const [mouseX, setMouseX] = useState(0);
+  const [mouseY, setMouseY] = useState(0);
+  const arrowRefs = useRef<HTMLDivElement[]>([]);
 
-  const handleTouch = (
-    rowIndex: number,
-    colIndex: number,
-    event: React.MouseEvent | React.TouchEvent<Element>
-  ) => {
-    const rect = (event.target as Element).getBoundingClientRect();
-    let x, y;
-    if ("clientX" in event) {
-      // MouseEvent
-      x = event.clientX - rect.left;
-      y = event.clientY - rect.top;
-    } else {
-      // TouchEvent
-      x = event.touches[0].clientX - rect.left;
-      y = event.touches[0].clientY - rect.top;
+  useEffect(() => {
+    const handleMouseMove = (e: MouseEvent) => {
+      setMouseX(e.clientX);
+      setMouseY(e.clientY);
+      console.log({ x: e.clientX, y: e.clientY });
+      console.log({ positionRef: arrowRefs.current });
+    };
+    document.body.addEventListener("mousemove", handleMouseMove);
+    return () => {
+      document.body.removeEventListener("mousemove", handleMouseMove);
+    };
+  }, []);
+
+  useEffect(() => {
+    // let angle;
+    // angle = Math.atan2(mouseY - 50, mouseX - 50) * (180 / Math.PI);
+    // const newRotation = [...rotations];
+    // // rowIndex와 colIndex 계산
+    // const rowIndex = Math.floor(mouseY / (window.innerHeight / rows));
+    // const colIndex = Math.floor(mouseX / (window.innerWidth / cols));
+    // console.log({ rowIndex, colIndex, mouseX, mouseY });
+    // newRotation[rowIndex][colIndex] = angle;
+    // setRotations(newRotation);
+    const newRotation = [...rotations];
+    for (let rowIndex = 0; rowIndex < rowNumber; rowIndex++) {
+      for (let colIndex = 0; colIndex < colNumber; colIndex++) {
+        const ref = arrowRefs.current[(rowIndex + 1) * colIndex];
+        const boundingBox = ref.getBoundingClientRect();
+        const cellCenterX = boundingBox.x + boundingBox.width / 2;
+        const cellCenterY = boundingBox.y + boundingBox.height / 2;
+        // 앵글
+        // 0 위
+        // 90 우
+        // 180 아래
+        // 270 좌
+        const angle =
+          (Math.atan2(
+            -cellCenterY,
+            document.body.clientWidth / 2 - cellCenterX
+          ) *
+            180) /
+          Math.PI;
+        // Math.atan2(mouseY - cellCenterY, mouseX - cellCenterX) *
+        // (180 / Math.PI);
+
+        console.log({
+          mousex: mouseX,
+          mousey: mouseY,
+          cellCenterY: cellCenterY,
+          cellCenterX: cellCenterX,
+          angle: angle,
+        });
+        // 각도를 0에서 360 사이의 값으로 변환합니다.
+        newRotation[rowIndex][colIndex] = (angle + 360) % 360;
+      }
     }
-    const angle = Math.atan2(y - 50, x - 50) * (180 / Math.PI) + 90;
-    const newRotation = Array(rows).fill(Array(cols).fill(angle));
-    setRotation(newRotation);
-  };
+    setRotations(newRotation);
+  }, [mouseX, mouseY]);
 
   return (
     <main className="flex flex-wrap justify-center h-screen w-screen bg-black">
-      {Array(rows)
+      {Array(rowNumber)
         .fill(0)
         .map((_, rowIndex) => (
           <div key={rowIndex} className="flex flex-wrap min-h-2 min-w-20">
-            {Array(cols)
+            {Array(colNumber)
               .fill(0)
               .map((_, colIndex) => (
                 <div key={colIndex} className="flex-1">
                   <div
-                    onTouchStart={(event) =>
-                      handleTouch(rowIndex, colIndex, event)
-                    }
+                    ref={(elem) => {
+                      if (elem) {
+                        arrowRefs.current[(rowIndex + 1) * colIndex] = elem;
+                      }
+                    }}
                   >
                     <Image
                       className="object-cover"
@@ -54,7 +96,7 @@ export default function Home() {
                       height={100}
                       priority
                       style={{
-                        transform: `rotate(${rotation[rowIndex][colIndex]}deg)`,
+                        transform: `rotate(${rotations[rowIndex][colIndex]}deg)`,
                       }}
                     />
                   </div>
